63d62613a4260486f6e24cc7f7c418985c31875d
David Olewski <david63210@gmail.com>
2017-05-02 16:37:42 +0200

    Readme

--- Assets/script/ScoreScript.cs
+++ Assets/script/ScoreScript.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
-
     public Text scoreText;
     public int score = 2147483647;
 
---
+++ README.md
@@ -0,0 +1,3 @@
+A Shoot Them Up for learn Unity 5.5
+
+# Shoot Them Up
